eb81b122679c263255f8f3b5701ce90c79fdb7c8
MaulanaKhisyam <103562745+MaulanaKhisyam@users.noreply.github.com>
2023-06-11 00:16:02 +0800

    Add files via upload

--- tugasss/app/src/main/java/com/example/tugasss/MainAdapter.kt
+++ tugasss/app/src/main/java/com/example/tugasss/MainAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.tugasss
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -7,34 +8,45 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import coil.transform.CircleCropTransformation
-import eu.tutorials.retrofitexmple.network.Character
+import com.example.tugasss.network.Character
 
 
-class MainAdapter(val characrersList: List<Character>) : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+//mendeklarasikan class MainAdapter untuk menerima parameter yang akan ditampilkan dalam RecyclerView
+class MainAdapter(private val charactersList: List<Character>?) : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
 
-    inner class MainViewHolder(private val itemView: View):RecyclerView.ViewHolder(itemView){
-        fun bindData(character: Character){
+    // class MainViewHolder yang mewariskan dari kelas RecyclerView.ViewHolder
+    // inner class untuk mengikat data dan menginisialisasi karakter
+    inner class MainViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // metode bindata yang menerima parameter character dari kelas Character
+        // yang mengikat data karakter ke tampilan dengan mengakses UI menggunnkan findViewById
+        fun bindData(character: Character) {
             val name = itemView.findViewById<TextView>(R.id.name)
-            val image = itemView.findViewById<TextView>(R.id.image)
+            val image = itemView.findViewById<ImageView>(R.id.image)
 
             name.text = character.name
-            image.load (character.image){
-                transformation(CircleCropTransformation())
+            // untuk mengatur gambar karakter dengan menggunakan library Coil untuk URL
+            image.load(character.image) {
+                transformations(CircleCropTransformation())
             }
         }
     }
-
+    // Method dipanggil jika baru dibuat
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
-        return MainViewHolder(layoutflater.from(parent.context).inflate(R.layout.rv_item, parent, false))
+        // layout yang diinflate menggunakan "LayoutInflater" yang dikembalikan sebagai inisiasi dari "MainViewHolder"
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.rv_item, parent, false)
+        return MainViewHolder(view)
     }
-
+    // method untuk mengembalikan jumlah item jika null, maka akan mengembalikan ke 0
     override fun getItemCount(): Int {
-        return characrersList.size
+        return charactersList?.size ?: 0
     }
-
+    // method yang dipanggil jika karakter perlu di bind ke ViewHolder.
+    // dan data karakter pada posisi tertentu dipanggil
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
-        holder.binData(characrersList[position])
+        charactersList?.get(position)?.let { character ->
+            holder.bindData(character)
+        }
     }
-
 }
 
--- tugasss/app/src/main/java/com/example/tugasss/network/Character.kt
+++ tugasss/app/src/main/java/com/example/tugasss/network/Character.kt
@@ -2,13 +2,20 @@ package com.example.tugasss.network
 
 import com.squareup.moshi.Json
 
+//membuat data class character dengan properti 'name' dan 'image'
 data class Character (
+    //@Json digunakan untuk memberi tahu library Moshi
     @Json(name="name")
     val name: String,
     @Json(name="image")
     val image: String
-)
-data class CharacterResponse(@Json(name="result")
-    val result : List<Character>) {
 
+)
+//method yang digunakan untuk merepresentasikan respon API yang mengandung daftar karakter
+data class CharacterResponse(@Json(name="results")
+//properti result digunakan menyimpan daftar karakter yang diterima dari respon API
+    val result : List<Character>) {
 }
+
+
+
